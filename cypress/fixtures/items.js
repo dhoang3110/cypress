a0d20f346041b8f2b226c43ee40b0adfbe55d85a
Dong Hoang <dhoang@nine.com.au>
2019-11-27 10:53:52 +1100

    inital

---
+++ cypress/fixtures/items.js
@@ -0,0 +1,27 @@
+module.exports = {
+    sites: {
+        NH: 'NH',
+        News: '9News',
+        Wwos: 'WWOS',
+        Finance: 'Finance',
+        Honey: 'Honey',
+        Coach: 'Coach")]',
+        Homes: 'Homes")]',  
+        Kitchen: 'Kitchen',
+        Style: 'Style',
+        Celebrity: 'Celebrity',
+        Travel: 'Travel',
+        NEC: 'NEC',
+        ShowSites: 'Show Sites',
+        Entertainment: 'Entertainment',
+        TechBlog: 'Tech Blog',
+        Now: '9Now',
+    },
+
+    articles: {
+        NH: 'https://press.uat.nine.com.au/item/0c2c7b4a-e85c-4547-8a71-54d2bb2b395e',
+        News: 'Vic train works to cause commuter pain',
+        NewsSearch: '/search?query=Vic%20train%20works%20to%20cause%20commuter%20pain',
+    }
+    
+}
---
+++ cypress/integration/editArticles.spec.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+const myItems = require('../fixtures/items')
+const expect = require('chai').expect
+import * as myActions from "../support/actions"
+
+
+describe('Test all sites dashboard', () => {
+  
+  before(() => {
+   myActions.logIn()
+  })
+
+  afterEach(() => {
+    //showAllSites()
+  })
+
+ it('Test edit article on 9News', () => {
+  myActions.selectSite(myItems.sites.News)
+  myActions.navigateArticle(myItems.sites.News)
+  myActions.editArticle()
+
+  // cy.wait(3000)
+
+ })
+
+ it.skip('TFake test', () => {
+  cy.server()
+  cy.route('OPTIONS','**/template/api/v1/**').as('getUsers')
+  //myActions.navigateArticle(myItems.sites.News)
+  cy.contains('Create New').click()
+  cy.contains('Article').click()
+  cy.wait('@getUsers')
+  // cy.wait(3000)
+
+ })
+
+})
